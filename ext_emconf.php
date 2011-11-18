b3890e775f86e4e4bd6b1cf27e8cd6b18b28ad0c
virgoule <virgoule@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-18 06:49:16 +0000

    Clean up ext_emconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rss_display/trunk@54453 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "rss_display".
 #
-# Auto generated 28-10-2011 09:55
+# Auto generated 18-11-2011 13:48
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -40,7 +40,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"eb74";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"d91e";s:17:"ext_localconf.php";s:4:"ad3f";s:14:"ext_tables.php";s:4:"7e70";s:14:"ext_tables.sql";s:4:"6e82";s:13:"locallang.xml";s:4:"c78f";s:16:"locallang_db.xml";s:4:"ff82";s:7:"rss.css";s:4:"fd37";s:19:"doc/wizard_form.dat";s:4:"f586";s:20:"doc/wizard_form.html";s:4:"d14a";s:14:"pi1/ce_wiz.gif";s:4:"d91e";s:27:"pi1/class.tx_rssdisplay_pi1.php";s:4:"ccdf";s:35:"pi1/class.tx_rssdisplay_pi1_wizicon.php";s:4:"9ba1";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"a946";s:24:"pi1/static/editorcfg.txt";s:4:"ce72";}',
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"a718";s:12:"ext_icon.gif";s:4:"0bfd";s:17:"ext_localconf.php";s:4:"3591";s:14:"ext_tables.php";s:4:"29ed";s:14:"ext_tables.sql";s:4:"20ad";s:24:"ext_typoscript_setup.txt";s:4:"2011";s:13:"locallang.xml";s:4:"1143";s:16:"locallang_db.xml";s:4:"041e";s:37:"Resources/Private/Templates/Feed.html";s:4:"b036";s:14:"doc/manual.pdf";s:4:"12d0";s:14:"doc/manual.sxw";s:4:"0b6a";s:31:"pi1/class.tx_rssdisplay_pi1.php";s:4:"86ca";s:39:"pi1/class.tx_rssdisplay_pi1_wizicon.php";s:4:"cc69";s:24:"pi1/static/editorcfg.txt";s:4:"89b0";}',
+	'suggests' => array(
+	),
 );
 
 ?>
