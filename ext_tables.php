e037782dd418d67bc6874609ffb82b6ed557f4b4
Fabien Udriot <fabien.udriot@ecodev.ch>
2014-01-21 11:22:41 +0400

    [TASK] Remove obsolete code
    Change-Id: I432791fed7653e7936ca2f75d9cc5c78790ea996
    Reviewed-on: https://review.typo3.org/26960
    Reviewed-by: Fabien Udriot
    Tested-by: Fabien Udriot

--- ext_tables.php
+++ ext_tables.php
@@ -1,63 +1,9 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
-$tempColumns = Array (
-	'tx_rssdisplay_feed' => Array (
-		'exclude' => 0,
-		'label' => 'LLL:EXT:rss_display/locallang_db.xml:tt_content.tx_rssdisplay_feed',
-		'config' => Array (
-			'type' => 'input',
-			'size' => '15',
-			'max' => '255',
-			'checkbox' => '',
-			'eval' => 'trim',
-			'wizards' => Array(
-				'_PADDING' => 2,
-				'link' => Array(
-					'type' => 'popup',
-					'title' => 'Link',
-					'icon' => 'link_popup.gif',
-					'script' => 'browse_links.php?mode=wizard',
-					'JSopenParams' => 'height=300,width=500,status=0,menubar=0,scrollbars=1'
-				)
-			)
-		)
-	),
-	'tx_rssdisplay_quantity' => Array (
-		'exclude' => 0,
-		'label' => 'LLL:EXT:rss_display/locallang_db.xml:tt_content.tx_rssdisplay_quantity',
-		'config' => Array (
-			'type' => 'input',
-			'size' => '30',
-			'default' => '10',
-			'eval' => 'int',
-		)
-	),
-	'tx_rssdisplay_descriptiondisplay' => Array (
-		'exclude' => 0,
-		'label' => 'LLL:EXT:rss_display/locallang_db.xml:tt_content.tx_rssdisplay_descriptiondisplay',
-		'config' => Array (
-			'type' => 'check',
-		)
-	),
-	'tx_rssdisplay_descriptionlength' => Array(
-		'exclude' => 0,
-		'label' => 'LLL:EXT:rss_display/locallang_db.xml:tt_content.tx_rssdisplay_descriptionlength',
-		'config' => Array (
-			'type' => 'input',
-			'size' => '30',
-			'eval' => 'int',
-		)
-
-	),
-);
-
-t3lib_div::loadTCA('tt_content');
-t3lib_extMgm::addTCAcolumns('tt_content',$tempColumns,1);
 
 $extensionName = t3lib_div::underscoredToUpperCamelCase($_EXTKEY);
 $pluginSignature = strtolower($extensionName) . '_pi1';
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature]='layout,select_key';
-#$TCA['tt_content']['types']['list']['subtypes_addlist']['rssdisplay_pi1']='tx_rssdisplay_feed, tx_rssdisplay_quantity, tx_rssdisplay_descriptiondisplay, tx_rssdisplay_descriptionlength';
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature]='pi_flexform';
 
 t3lib_extMgm::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForm/feed.xml');
--- ext_tables.sql
+++ ext_tables.sql
@@ -1,17 +1,6 @@
 #
 # Table structure for table 'tt_content'
 #
-CREATE TABLE tt_content (
-	tx_rssdisplay_feed tinytext NOT NULL,
-	tx_rssdisplay_descriptiondisplay int(11) DEFAULT '0' NOT NULL,
-	tx_rssdisplay_quantity int(11) DEFAULT '0' NOT NULL,
-	tx_rssdisplay_descriptionlength int(11) DEFAULT '0' NOT NULL
-	#tx_rssdisplay_cache_lifetime int(11) DEFAULT '3600' NOT NULL
-);
-
-#
-# Table structure for table 'tt_content'
-#
 CREATE TABLE tx_rssdisplay_cache (
     id int(11) unsigned NOT NULL auto_increment,
     identifier varchar(250) DEFAULT '' NOT NULL,
