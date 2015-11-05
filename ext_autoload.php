233c90b15449bd9ca12f35cf2929923e42a173ae
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-11-05 17:54:19 +0100

    [TASK] Remove unnecessary ext_autoload.php
    Resolves #6

--- ext_autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-$extensionClassesPath = t3lib_extMgm::extPath('rss_display') . 'Classes/';
-return array(
-	'tx_rssdisplay_controller_feedcontroller' => $extensionClassesPath . 'Controller/FeedController.php',
-);
-?>
