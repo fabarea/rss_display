ff6d18f6a4d1f7f741da3f6aa7dcd785753ecaad
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-11-06 16:10:38 +0100

    [TASK] Set extension compatible 6.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' =>
     array (
-      'typo3' => '7.5.0-7.99.99',
+      'typo3' => '6.2.0-7.99.99',
       'fluid' => '',
       'extbase' => '',
     ),
