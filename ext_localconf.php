61a3b79916237dd2fbccb1931472a7ff22867305
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-08-16 09:52:42 +0200

    [REFACTOR] Change TS autoloading strategy
    Resolves #14

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,27 @@
 <?php
 if (!defined('TYPO3_MODE')) die ('Access denied.');
 
+// Define whether USER or USER_INT.
 $pluginType = 'USER_INT';
 $configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['rss_display']);
 if (!empty($configuration['plugin_type'])) {
     $pluginType = $configuration['plugin_type'];
+}
+
+// Define whether to automatically load TS.
+if (false === isset($configuration['autoload_typoscript']) || true === (bool)$configuration['autoload_typoscript']) {
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+        'rss_display',
+        'constants',
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:rss_display/Configuration/TypoScript/constants.txt">'
+    );
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+        'rss_display',
+        'setup',
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:rss_display/Configuration/TypoScript/setup.txt">'
+    );
 }
 
 // Configure Extbase plugin
@@ -20,6 +37,7 @@ $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['rssdisplay'] = array(
 //	'options' => array(),
     'groups' => array('all', 'rssdisplay')
 );
+
 
 
 # Install PSR-0-compatible class autoloader for SimplePie Library in Resources/PHP/SimplePie
--- ext_tables.php
+++ ext_tables.php
@@ -11,3 +11,9 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['rssdisplay_p
     'Pi1',
     'Fetch and display a RSS feed'
 );
+
+// Possible Static TS loading
+$configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['rss_display']);
+if (true === isset($configuration['autoload_typoscript']['value']) && true === (bool)$configuration['autoload_typoscript']['value']) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('rss_display', 'Configuration/TypoScript', 'Vidi Frontend: generic List Component');
+}
