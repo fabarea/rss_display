f7e02a39ae68f74a0e0b65ea114b02a6912e9da0
Fabien Udriot <fabien@omic.ch>
2021-01-26 19:03:39 +0100

    Release 5.0.1 - <https://github.com/fabarea/rss_display/compare/5.0.0...5.0.1>

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'RSS Feed Display',
     'description' => 'Fetch a RSS / Atom Feed and display its content on the Frontend.',
     'category' => 'plugin',
-    'version' => '5.1.0-dev',
+    'version' => '5.0.1',
     'state' => 'stable',
     'author' => 'Fabien Udriot',
     'author_email' => 'fabien@ecodev.ch',
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
         [
             'depends' =>
                 [
-                    'typo3' => '9.5.0-10.4.99',
+                    'typo3' => '10.4.0-10.4.99',
                 ],
             'conflicts' =>
                 [
