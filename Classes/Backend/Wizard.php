e4f2bd5865e3be46a9e5b02dd6963ded95cb0b66
Stig Nørgaard Færch <snf@dkm.dk>
2021-10-29 16:06:17 +0200

    [BUGFIX] New Content Element Wizard broken

--- Classes/Backend/Wizard.php
+++ Classes/Backend/Wizard.php
@@ -8,36 +8,50 @@ namespace Fab\RssDisplay\Backend;
  * LICENSE.md file that was distributed with this source code.
  */
 
+use TYPO3\CMS\Backend\Wizard\NewContentElementWizardHookInterface;
+
 
 /**
  * Class that adds the wizard icon.
  */
-class Wizard
+class Wizard implements NewContentElementWizardHookInterface
 {
 
     /**
      * Processing the wizard items array
      *
-     * @param array $wizardItems : The wizard items
-     * @return array
-     * @throws \BadFunctionCallException
+     * @param array $wizardItems The wizard items
      */
-    public function proc($wizardItems)
+    public function manipulateWizardItems(&$wizardItems, &$parentObject)
     {
-        $wizardItems['plugins_tx_rssdisplay_pi1'] = array(
-            'iconIdentifier' => 'plugins_tx_rssdisplay_pi1_wizard', // <- see ext_tables.php
-            'title' => $this->getLanguageService()->sL('LLL:EXT:rss_display/Resources/Private/Language/locallang.xlf:wizard.title'),
-            'description' => $this->getLanguageService()->sL('LLL:EXT:rss_display/Resources/Private/Language/locallang.xlf:wizard.description'),
-            'params' => '&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=rssdisplay_pi1'
-        );
-
-        return $wizardItems;
+        # Find index of last plugin.
+        foreach (array_keys($wizardItems) as $index => $key) {
+            if(strpos($key, 'plugin') === 0) {
+                $found = true;
+            } elseif (isset($found)) {
+                array_splice($wizardItems, $index, 0, [
+                    'plugins_tx_rssdisplay_pi1' => [
+                        'iconIdentifier' => 'plugins_tx_rssdisplay_pi1_wizard', // <- see ext_tables.php
+                        'iconIdentifier' => 'ext-news-wizard-icon', // <- see ext_tables.php
+                        'title' => $this->getLanguageService()->sL('LLL:EXT:rss_display/Resources/Private/Language/locallang.xlf:wizard.title'),
+                        'description' => $this->getLanguageService()->sL('LLL:EXT:rss_display/Resources/Private/Language/locallang.xlf:wizard.description'),
+                        'saveAndClose' => false,
+                        'tt_content_defValues' => [
+                            'CType' => "list",
+                            'list_type' => "rssdisplay_pi1"
+                        ],
+                        'params' => '&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=rssdisplay_pi1'
+                    ]
+                ]);
+                break;
+            };
+        }
     }
 
     /**
      * @return \TYPO3\CMS\Core\Localization\LanguageService
      */
-    protected function getLanguageService()
+    protected function getLanguageService(): \TYPO3\CMS\Core\Localization\LanguageService
     {
         return $GLOBALS['LANG'];
     }
