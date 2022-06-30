22fd546e8fcf7edbf2849a6c16953b835186303d
Sybille Peters <sypets@gmx.de>
2022-06-30 20:40:03 +0200

    [TASK] Move registerPlugin to TCA/Overrides
    Per convention, registerPlugin and settting
    TCA directly should be in Configuration/TCA.
    
    Resolves: #71

--- ext_tables.php
+++ ext_tables.php
@@ -1,16 +1,6 @@
 <?php
 if (!defined('TYPO3_MODE')) die ('Access denied.');
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['rssdisplay_pi1'] = 'layout, select_key, pages, recursive';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['rssdisplay_pi1'] = 'pi_flexform';
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('rssdisplay_pi1', 'FILE:EXT:rss_display/Configuration/FlexForm/feed.xml');
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'rss_display',
-    'Pi1',
-    'Fetch and display a RSS feed'
-);
 
 // Possible Static TS loading
 $configuration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('rss_display');
