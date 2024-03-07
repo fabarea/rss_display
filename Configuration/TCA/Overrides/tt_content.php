e21c309ba370534c5af2dbcc75ce0b333bf322bb
Sefux <sfuchs@projektkater.de>
2024-03-07 17:05:24 +0100

    [BUGFIX] Wrong path for Configuration/TCA/Overrides (see also #79)

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,12 +1,13 @@
 <?php
 
-defined('TYPO3_MODE') or die();
+defined('TYPO3') or die();
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'rss_display',
     'Pi1',
     'Fetch and display a RSS feed'
 );
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('rssdisplay_pi1', 'FILE:EXT:rss_display/Configuration/FlexForm/feed.xml');
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['rssdisplay_pi1'] = 'layout, select_key, pages, recursive';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['rssdisplay_pi1'] = 'pi_flexform';
